74de3aeadd8be04e9f4d4f583a7887bd0eccb561
evelyn s <evelyn_stng@protonmail.com>
2024-03-16 23:49:58 +0100

    sanitize firmware for atmega and rp2040 version

---
+++ keyboards/scattereddrifter/endeavour/atmega32u2/config.h
@@ -0,0 +1,32 @@
+/*
+ * Derivate of kb_elmo's software > elmo.space 
+Copyright 2021 fabianstabusiness@protonmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Keyboard Matrix Assignments */
+// #define MATRIX_ROW_PINS { B0, B1, B2, B3  }
+// #define MATRIX_COL_PINS { B7, C7, B5, B4, C6, D4, D3, D2, D5, D6 }
+// #define UNUSED_PINS
+
+/* COL2ROW, ROW2COL */
+
+/* CAPS LED */
+#define LED_CAPS_LOCK_PIN B1
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
---
+++ keyboards/scattereddrifter/endeavour/atmega32u2/rules.mk
@@ -0,0 +1,6 @@
+# options for atmega32u2
+MCU = atmega32u2
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+# those mostly moved to info.json
+SLEEP_LED_ENABLE = no
